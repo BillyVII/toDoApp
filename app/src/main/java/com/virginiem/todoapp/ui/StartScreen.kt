534a0c9e9d0e703561acef43d2ecbc8d1e852507
Billy <105440324+BillyVII@users.noreply.github.com>
2024-03-21 22:22:01 +0100

    listof

--- app/src/main/java/com/virginiem/todoapp/ui/StartScreen.kt
+++ app/src/main/java/com/virginiem/todoapp/ui/StartScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
@@ -17,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -27,46 +29,43 @@ fun StartScreen (startScreenViewModel: StartScreenViewModel = viewModel(), modif
 
     val startUiState by startScreenViewModel.uiState.collectAsState()
 
-    var text by remember { mutableStateOf("")}
     Column (modifier = modifier
         .fillMaxSize()
         .padding(36.dp),
         verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.End) {
         Text(text = "toto")
-        EditTextField()
-        Button(
-            onClick = { startScreenViewModel.AddEntree() }) {
-            Text(text = "toto")
+        EditTextField(viewModel = startScreenViewModel)
 
-        }
     }
 
 }
 @Composable
-fun EditTextField(){
-    var text by remember { mutableStateOf("") }
+fun EditTextField(viewModel: StartScreenViewModel){
+    var text by remember { mutableStateOf("")}
     OutlinedTextField(
         value = text,
-        onValueChange = { text = it},
+        onValueChange = {
+            text = it
+        },
         label = { Text(text = "label")},
         keyboardOptions = KeyboardOptions.Default.copy(
-            imeAction = ImeAction.Done
-        )
+            imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Text
+        ),
+        keyboardActions = KeyboardActions()
     )
+
+    ButtonClicked(word = text, viewModel = viewModel )
 
 }
 
-//fun AddEntree(){
-//    val entreesList = mutableListOf<String>()
-//    entreesList.add("toto")
-//    entreesList.add("titi")
-//   entreesList.add("tata")
-//
-//    for (entree in entreesList) {
-//        println(entree)
-//    }
-//}
+@Composable
+fun ButtonClicked(word:String, viewModel: StartScreenViewModel){
+    Button(onClick = { viewModel.addEntree(word) }) {
+
+    }
+}
 
 @Preview
 @Composable
--- app/src/main/java/com/virginiem/todoapp/ui/StartScreenViewModel.kt
+++ app/src/main/java/com/virginiem/todoapp/ui/StartScreenViewModel.kt
@@ -1,5 +1,9 @@
 package com.virginiem.todoapp.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.virginiem.todoapp.ui.model.Entree
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -10,15 +14,21 @@ class StartScreenViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(Entree())
     val uiState: StateFlow<Entree> = _uiState.asStateFlow()
 
-    val entreesList = mutableListOf<String>()
-    fun AddEntree(){
+    private val entreesList = mutableListOf<String>()
+    private var word by mutableStateOf("")
+    fun addEntree(wordAdd: String){
         // val entreesList = mutableListOf<String>()
         entreesList.add("toto")
         entreesList.add("titi")
         entreesList.add("tata")
+        entreesList.add(wordAdd)
 
         for (entree in entreesList) {
             println(entree)
         }
+    }
+
+    fun updateWord (newWord:String){
+        word = newWord
     }
 }
